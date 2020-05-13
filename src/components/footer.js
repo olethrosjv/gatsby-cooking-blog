87dc2a7c9137bbe9575a17959994679032acbd0c
olethrosjv <chefjoevaughn@gmail.com>
2020-05-12 17:47:19 -0700

    Added Footer component, started Nav component, changed main image(neumorphic) and background color

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+const Footer = () => (
+
+    <footer>
+        © {new Date().getFullYear()}, Created by
+        {` `}
+        <a href="https://joe-vaughn-portfolio.netlify.app/">Joe Vaughn</a>
+    </footer>
+
+)
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,16 +9,20 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <p>Welcome to my cooking site.</p>
+    <p>Now go make something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `3rem`,
+  borderRadius: `100px`, background: `#eeeeee`, boxShadow: `20px 20px 50px #121212` }}>
       <Image />
     </div>
+    <nav>
     <ul>
       <li><Link to="/page-2/">Go to page 2</Link></li>
       <li><Link to="/about/">About Us</Link></li>
+      <li><Link to="/awesome/">Awesome</Link></li>
+      <li><Link to="/another/">Another</Link></li>
     </ul>
-    
+    </nav>
 
   </Layout>
 )
